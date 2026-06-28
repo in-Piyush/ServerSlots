b6c20a6a64a87e49c2866bf1de2aa138ca0d1ad3
Piyush <deoxy004@gmail.com>
2026-06-28 15:36:51 +0530

    Folia Support

--- src/main/java/in/piyush/serverSlots/ServerSlots.java
+++ src/main/java/in/piyush/serverSlots/ServerSlots.java
@@ -1,5 +1,6 @@
 package in.piyush.serverSlots;
 
+import com.tcoded.folialib.FoliaLib;
 import in.piyush.serverSlots.commands.serverslotcommand;
 import org.bstats.bukkit.Metrics;
 import org.bukkit.event.Listener;
@@ -7,8 +8,11 @@ import org.bukkit.plugin.java.JavaPlugin;
 
 public final class ServerSlots extends JavaPlugin  implements Listener {
 
+    private FoliaLib foliaLib;
+
     @Override
     public void onEnable() {
+        this.foliaLib = new FoliaLib(this);
         getLogger().info("Plugin Enabled");
          getCommand("serverslots").setExecutor(new serverslotcommand());
         int pluginId = 31309;
@@ -19,5 +23,9 @@ public final class ServerSlots extends JavaPlugin  implements Listener {
     @Override
     public void onDisable() {
         getLogger().info("Plugin Disabled");
+    }
+
+    public FoliaLib getFoliaLib() {
+        return foliaLib;
     }
 }
